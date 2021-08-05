2ad32e1ad8ffd6e2bb91bcc6be0cf110820bf995
Levi-Opunga <leviopunga@gmail.com>
2021-08-05 13:07:55 +0300

    correcting java script

--- js/script.js
+++ js/script.js
@@ -8,8 +8,10 @@ $(document).ready(function() {
             .match(
                 "[A-Za-z0-9._%+-]{2,}@[a-zA-Z]{1,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})"
             ) &&
-            $("textarea").val() > ""
-        )
-            alert("Thank you for contacting us");
+            $("textarea").val() > "" &&
+            alert("Your message has been received")
+        ) {
+            window.open("../Thankyou.html");
+        }
     });
 });
