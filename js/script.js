04509e5dc453c805acfa2f5a077d5fea572fed4d
Levi-Opunga <leviopunga@gmail.com>
2021-08-05 12:48:31 +0300

    added java script for contact us page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    $("form".submit());
+    $("#submit").click(function() {
+        $("form").submit();
+        if (
+            $("#name").val() > "" &&
+            $("#email")
+            .val()
+            .match(
+                "[A-Za-z0-9._%+-]{2,}@[a-zA-Z]{1,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})"
+            ) &&
+            $("textarea").val() > ""
+        )
+            alert("Thank you for contacting us");
+    });
 });
