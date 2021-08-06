bc4b3549d341b2074fdf2ac0b3aff3e2cdfe0c4e
Levi-Opunga <86954179+Levi-Opunga@users.noreply.github.com>
2021-08-06 03:03:17 +0300

    Levi/homepage (#10)
    * Francis/aboutus (#1)
    
    * Edited license
    
    * Edited license
    
    * changes
    
    * peter changes
    
    * Added nyashinski pic and link to hapotu
    
    * Styled the background image to center Nyashinkski
    
    * commit (#6)
    
    * added footer and header display size optimisation, created contact us page and added header to it, created thankyou page
    
    * added contact us link to headers in different pages
    
    * added contact us form, aand fized label
    
    * added footer to contact us
    
    * added created a recommendations page
    
    * added header and footer to thank you page
    
    * removed a misplaced letter on top of doctype declaration
    
    * added footer and header display size optimisation, created contact us page and added header to it, created thankyou page
    
    * added contact us link to headers in different pages
    
    * added contact us form, aand fized label
    
    * added footer to contact us
    
    * added created a recommendations page
    
    * added header and footer to thank you page
    
    * Update page3.html
    
    added nyashinski
    
    * added header and footer to thank you page
    
    * corrected nyashinski image
    
    * added java script for contact us page
    
    * correcting java script
    
    * added fonts
    
    * made top header image resopnsive
    
    * made top header image resopnsive
    
    * made top header image resopnsive
    
    * made top header image resopnsive
    
    * fixed redirect link
    
    * incorporated page 4 by francis
    
    * added readme and license details
    
    * corrected navbar link names
    
    * final site
    
    * final site
    
    Co-authored-by: FrancisFlow <81323227+FrancisFlow@users.noreply.github.com>
    Co-authored-by: FrancisFlow <iamngigikaranja1@aol.com>

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#submit").click(function() {
+        $("form").submit();
+        if (
+            $("#name").val() > "" &&
+            $("#email")
+            .val()
+            .match(
+                "[A-Za-z0-9._%+-]{2,}@[a-zA-Z]{1,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})"
+            ) &&
+            $("textarea").val() > ""
+        ) {
+            window.open("./Thankyou.html");
+            window.location.reload();
+        }
+    });
+});
